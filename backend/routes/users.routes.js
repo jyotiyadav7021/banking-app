030e1621fe406362fcd86a550de46b4b1c8bb8ad
JyotiYadav <57616116+jyotiyadav7021@users.noreply.github.com>
2025-05-23 23:03:51 +0530

    new

---
+++ backend/routes/users.routes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+
+router.post("/",(req,res)=>{
+    res.status(200).json({
+        message:"Users requested"
+    })
+});
+
+module.exports = router;
